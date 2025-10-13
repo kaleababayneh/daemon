388fb1f4d4b7df3f391b1ae24cadb39a4bfb8786
itu-itis21-gizaw21 <gizaw21@itu.edu.tr>
2025-10-13 20:20:55 +0300

    clear out the readme

--- zk-scripts/test.ts
+++ zk-scripts/test.ts
@@ -5,10 +5,6 @@ const { viem } = await network.connect();
 const ACCOUNT_ADDR = "0xa16E02E87b7454126E5E10d957A927A7F5B5d2be"; 
 
 async function main() {
-
-
-  
-   
     const account = await viem.getContractAt("SmartAccount", ACCOUNT_ADDR);
     const count = await account.read.count();
     console.log("Smart Account Count:", count);
